abb59c7e82a67e3aa0b5515da3e571b3098d3ee0
pedro <pcosta@alunos.utfpr.edu.br>
2024-09-11 18:46:30 -0300

    commit codigo cliente

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -10,7 +10,7 @@ endereco_servidor_adm = ('localhost', 5001)
 # | op (int 4 bytes)| jogo selecionado (int 4 bytes)  |  #
 # | size (int 4 bytes) | nome (string utf-8 size bytes)| #
 # ====================================================== #
-            #        #
+            #        #                  
             #   ||   #
             #   ||   #
             #  \||/  #
@@ -45,8 +45,7 @@ endereco_servidor_adm = ('localhost', 5001)
 # ======================================= #
 
 
-def printa_mapa_jogador(mapa):
-    print("Mapa do jogador")
+def printa_mapa(mapa):
     print("  0 1 2 3 4 5 6 7 8 9")
     for i in range(10):
         print(i, end=" ")
@@ -54,15 +53,6 @@ def printa_mapa_jogador(mapa):
             print(mapa[i*10 + j], end=" ")
         print()
 
-
-def printa_mapa_adversario(mapa):
-    print("Mapa do adversário")
-    print("  0 1 2 3 4 5 6 7 8 9")
-    for i in range(10):
-        print(i, end=" ")
-        for j in range(10):
-            print(mapa[i*10 + j], end=" ")
-        print()
 
 
 def main():
@@ -86,6 +76,7 @@ def main():
         socket_sala.connect((mensagem, port))   ### Conecta a sala de jogo
         print("Conectado a sala de jogo")
         print("Aguarde o inicio do jogo")
+
         conexão = True
         while conexão:
             op, error, mapa_jogador, mapa_adversario = struct.unpack('!II100I100I', socket_sala.recv(808))
@@ -93,24 +84,24 @@ def main():
                 print("Erro: ", error)
                 conexão = False
             elif(op == 1):
-                printa_mapa_jogador(mapa_jogador)
+                printa_mapa(mapa_jogador)
                 orientacao = int(input("Digite 0 para orientação original e 1 para invertida: "))
                 coluna = int(input("Digite a posição na coluna: "))
                 linha = int(input("Digite a posição na linha: "))
                 socket_sala.send(struct.pack('!IIII', 1, coluna, linha, orientacao, len(nome), nome))
             elif(op == 2):
-                printa_mapa_jogador(mapa_jogador)
-                printa_mapa_adversario(mapa_adversario)
+                printa_mapa(mapa_jogador)
+                printa_mapa(mapa_adversario)
                 coluna = int(input("Digite a posição na coluna para realizar um disparo: "))
                 linha = int(input("Digite a posição na linha para realizar um disparo: "))
                 socket_sala.send(struct.pack('!IIII', 2, coluna, linha, 0, len(nome), nome))
             elif(op == 3):
-                printa_mapa_jogador(mapa_jogador)
-                printa_mapa_adversario(mapa_adversario)
+                printa_mapa(mapa_jogador)
+                printa_mapa(mapa_adversario)
                 print("O jogo acabou, você ganhou")
                 conexão = False
             elif(op == 4):
-                printa_mapa_jogador(mapa_jogador)
-                printa_mapa_adversario(mapa_adversario)
+                printa_mapa(mapa_jogador)
+                printa_mapa(mapa_adversario)
                 print("O jogo acabou, você perdeu")
                 conexão = False
