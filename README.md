791c60cd8dda4c97cc2cd4f538c4d0ce8f9eb007
pedro <pcosta@alunos.utfpr.edu.br>
2024-09-12 01:58:16 -0300

    polimento servidor_adm e preenchimento do readme.md

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Servidor_Hospedagem_Jogos
-Sistema distribuido para hospedagem de jogos, clientes so conectam com o servidor de comunicação e este se conecta ao servidor de processamento, onde o jogo acontece.
+### Servidor_Hospedagem_Jogos
+Sistema distribuido para hospedagem de salas de jogos, cliente solicita para um servidor mestre o endereço de uma sala para ele jogar, este servidor o envia o endereço
+e então ele se conecta neste endereço para jogar batalha naval(Jogo implementado para o Projeto), o sistema funciona utilizando os seguintes processos principais.
+
+# Servidor de processamento
+O servidor de processamento solicita o numero de uma porta inicial X pelo terminal, inicia N threads salas, cada sala se conecta ao servidor mestre, as salas tentarão inciar um socket que espera conexões de clientes nas portas X até X+N-1, cada sala então avisa ao servidor Mestre o endereço vinculado a porta que está aguardando conexões de clientes, quando dois clientes se conectarem na sala, o jogo é iniciado, clientes trocam mensagems com a sala até o jogo finalizar. Após o jogo finalizar a sala avisa o servidor Mestre o resultado do jogo e tambem indica que está livre para clientes se conectarem.
+
+# Cliente
+Cliente se conecta ao servidor Mestre e solicita uma sala, ao receber o endereço da sala ele encerra a conexão com o servidor mestre, o cliente então se conecta no socket da sala e espera/começa o jogo, clietes escolhem aonde querem inserir os navios e aonde irão atirar, após acabar o jogo o processo acaba.
+
+# Servidor Mestre ou Servidor de Adminitração
+O servidor mestre possui dois sockets que espeream conexões, um que espera conexões de salas e cria uma thread para lidar com ela, inicializando a sala antes do primeiro jogo e salvando seu endereço em um objeto sala e o colocando em uma fila salas, e após cada jogo acabar atualiza o estado do objeto sala. O segundo socket que espera conexões do servidor de Adminitração lida com os clientes, quando um cliente se conecta uma thread é iniciada para lidar com ele, a thread busca uma sala livre para este cliente se conectar, utilizando a lista salas, após encontrar uma sala livre, atualiza o estado da sala e envia seu endereço para o cliente, após isso a thread que lida com este cliente é finalizada e os sockets são fechados. Tanto a escrita no estado da sala em sua inicialização quanto a busca e atualização do valor na busca da sala para o cliente, são protegidas em uma região critica usando um semaforo, mantendo assim consistencia dos dados.
+
--- servidor_administração/servidor_adm.py
+++ servidor_administração/servidor_adm.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import struct
 
-semaforo1 = threading.Semaphore(1)
+semaforo1 = threading.Semaphore(1)  ### Semaforo utilizado na região critica para leitura e escrita da lista de salas
 
 lista_salas = []
 lista_thrads_servidor = []
@@ -27,7 +27,7 @@ lista_thrads_servidor = []
 # | size (int 4 bytes) | nome (string utf-8 size bytes)| #
 # ====================================================== #
 
-### Servidor de processamento -> Servidor de administração
+### Sala do servidor de processamento -> Servidor de administração
 
 # ============================================================ #
 # | op (int 4 bytes)| port (int 4 bytes)| error (int 4 bytes)| #
@@ -43,66 +43,69 @@ class Sala:
         self.estado = 0
         self.id = id
         
+    ### Sala do servidor de processamento -> Servidor de administração
 
-def thread_handle_sala(conn, addr):             
-    op, port, error, size = struct.unpack('!IIII', conn.recv(16))   ### Ponto 2 sala op == 1 pois indica que uma sala acabou de ser aberta
-    ip = conn.recv(size).decode()       # Decodifica o ip para utf-8
-    
+    # ============================================================ #
+    # | op (int 4 bytes)| port (int 4 bytes)| error (int 4 bytes)| #    ### Nota: Pode-se reparar que as salas enviam mensagens para o servidor de administração
+    # | size (int 4 bytes) |    ip (string utf-8 size bytes)     | #    ### mas o servidor de administração não envia mensagens para as salas
+    # ============================================================ #
+
+def thread_handle_sala(conn, addr):     ### Thread é iniciada quando uma sala se conecta ao socket que esperá conexão do servidor de processamento
+
+    op, port, error, size = struct.unpack('!IIII', conn.recv(16))   ### Recebe e desempacota a mensagem da sala
+                                                                    ### Indicando a porta que está esperando conexões de clientes
+    ip = conn.recv(size).decode()                                   ### Recebe o ip do socket que aguarda conexões de clientes
     print("Sala de ip: ", ip, " e porta: ", port, " conectada\n, operação: ", op, "\n")
 
-    sala_atual = Sala(ip, port, port)   # Cria o objeto sala da sala que acabou de se conectar
-                                                    
+    
+    sala_atual = Sala(ip, port, port)   # Cria o objeto sala da sala que acabou de se conectar                                     
     semaforo1.acquire()             # ================região critica================== #
     lista_salas.append(sala_atual)  #   # Adiciona o objeto sala na lista de salas #   #
     semaforo1.release()             # ================região critica================== #    
     
+    ### Esta thread agora ficará esperando atualizações da sala
     while True:
-        ### Recebe uma mensagem da sala indicando que o jogo acabou op == 1 pois indica que jogo acabou
-        op, resultado, erro, size = struct.unpack('!IIII', conn.recv(16))
-        nome = conn.recv(size).decode()     ### Recebe o nome do jogador que venceu a partida
+
+        ### Recebe uma mensagem da sala indicando que o jogo acabou
+        op, resultado, erro, size = struct.unpack('!IIII', conn.recv(16))   ### Recebe e desempacota a mensagem enviada pela sala
+        nome = conn.recv(size).decode()                                     ### Recebe o nome do jogador que venceu a partida
 
 
-        ### Utilizado op para verificar se o jogo acabou por qual motivo, se foi uma vitória(op == 1) então utiliza sala_atual.id
-        ### e nome do jogador vencedor para salvar no log e fazer uma leaderboard
-        if resultado == 1:
+        ### Utilizado op para verificar se o jogo acabou por qual motivo, se foi uma vitória ou erro
+        ### Utiliza o endereço da sala e nome do jogador vencedor para salvar no log e fazer uma leaderboard
+        if resultado == 1:  ### Indica que o jogo acabou por vitória
             print("Jogo acabou, o jogador ", nome, " venceu a partida na sala ", sala_atual.id,"\n")
-        else:
+        else:               ### Indica que o jogo acabou por erro
             print("Jogo da sala: ", sala_atual.id, "encerrou por motivo de erros, codigo do erro:",erro,"\n")
 
-        ###
 
-        ### Marca a sala como disponível 
+        ### Marca a sala como disponível para um novo jogo
         semaforo1.acquire()         # ================região critica================== #
         sala_atual.estado = 0       #           # marca a sala como vazia #            #
         semaforo1.release()         # ================região critica================== #
         
-def thread_salas():
+def thread_salas():                     ### Thread que espera conexões de salas do servidor de processamento
+    ### Cria o socket que espera as salas do servidor de processamento se conectarem
     socket_salas = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_salas.bind(('127.0.0.1', 5000))
     socket_salas.listen()
     
     while True:
-        conn, addr = socket_salas.accept()  ### Ponto 1 sala
+        
+        conn, addr = socket_salas.accept()  ### Um cliente sala conseguiu se consectar ao socket, agora chama um thread para lidar com ele
         thread_handle = threading.Thread(target=thread_handle_sala, args=(conn, addr))
-        thread_handle.start()
+        thread_handle.start()               ### Inicia a thread que irá lidar com a sala
         lista_thrads_servidor.append(thread_handle)
 
 
-def procura_sala_vaga():        # Toda essa função ocorre dentro de uma região critica # em um sistema poderia utizar varias listas de salas, na qual cada
-                                # lista de salas teria um semaforo separado, assim evitando a ocorrencia de varios clientes ficarem esperando uma resposta
-                                # de salas livres, aplicar uma lista circular que atualização a posição atual sempre que uma sala for completamente cheia
-                                # para a proxima sala, que provavelmente estará vazia, pois a lista iria preencher linearmente as salas com jogadores.
+def procura_sala_vaga():        ### Toda essa função ocorre dentro de uma região critica que é protegida por um semaforo, procura uma sala vaga para um cliente
     for sala in lista_salas:    
         if sala.estado == 1:    ### Procura sala que possuem um jogador em espera
             return sala
     for sala in lista_salas:
         if sala.estado == 0:    ### Procura sala vazias
             return sala
-    return None     ### Retorna None explicitamente quando não encontrar uma sala disponível
-
-
-
-
+    return None     ### Retorna None quando não encontrar uma sala disponível
 
 ### Cliente -> Servidor de administração
 
@@ -122,8 +125,8 @@ def procura_sala_vaga():        # Toda essa função ocorre dentro de uma regiã
 # | op (int 4 bytes)| port (int 4 bytes)| error (int 4 bytes)| #
 # | size (int 4 bytes) |        ip (string size bytes)       | #
 # ============================================================ #
-
-def thread_handle_cliente(conn, addr):  
+                                            ### Thread que é iniciada quando um cliente se conecta ao servidor de administração solicitando uma sala
+def thread_handle_cliente(conn, addr):      ### Está thread então envia para o cliente a sala encontrada ou uma mensagem de erro
     
     ### Recebe a mensagem do cliente
         # ====================================================== #
@@ -175,20 +178,21 @@ def thread_handle_cliente(conn, addr):
     conn.send(mensagem)         ### Envia a mensagem para o cliente, que pode ser um ip ou uma mensagem de erro
     conn.close()
 
-def thread_clientes():
+def thread_clientes():                      ### Thread que espera conexões de clientes solicitando uma sala
     socket_clientes = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_clientes.bind(('localhost', 5001))                   
     socket_clientes.listen()                                ### Espera clientes conectarem para pedir o endereço de uma sala vaga
     while True:
-        conn, addr = socket_clientes.accept()               ### Cliente conectou, agora chama um thread para lidar com ele
+        conn, addr = socket_clientes.accept()               ### Cliente conectou
         thread_cli = threading.Thread(target=thread_handle_cliente, args=(conn, addr))
-        thread_cli.start()
+        thread_cli.start()                                  ### Inicia uma thread para lidar com ele
+
 
 def ligar_servidor():
     thread_sala_espera = threading.Thread(target=thread_salas)
-    thread_sala_espera.start()
+    thread_sala_espera.start()                                          ### Inicia a thread que espera as salas do servidor de processamento se conectarem
     thread_espera_clientes = threading.Thread(target=thread_clientes)
-    thread_espera_clientes.start()
+    thread_espera_clientes.start()                                      ### Inicia a thread que espera os clientes se conectarem solocitando uma sala
     while True:
         pass
 
